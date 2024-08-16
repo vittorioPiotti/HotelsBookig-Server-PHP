129e33bb4fbda898e7ce3fc19735be5b77471acc
vittorioPiotti <vittoriopiotti.vp@gmail.com>
2024-08-16 12:47:45 +0200

    Update index.php

--- project/index.php
+++ project/index.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * Gestione Hotels Server v1.0.0 (https://github.com/vittorioPiotti/Gestione-Hotels-Server/releases/tag/1.0.0)
+ * Copyright 2024 Vittorio Piotti
+ * Licensed under GPL-3.0 (https://github.com/vittorioPiotti/Gestione-Hotels-Server/blob/main/LICENSE.md)
+ */
 
 require_once __DIR__ . '/src/autoloader.php';
 
@@ -12,4 +17,4 @@ $fapi->listener();
 
 
 
-?>
+?>
